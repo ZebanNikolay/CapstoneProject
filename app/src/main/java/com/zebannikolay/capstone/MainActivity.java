8c5a29c42698b6afdf2e8167576a336bfc991967
ZebanNikolay <zebannikolay@gmail.com>
2018-07-07 21:09:18 +0700

    Add ViewPager

--- app/src/main/java/com/zebannikolay/capstone/MainActivity.java
+++ app/src/main/java/com/zebannikolay/capstone/MainActivity.java
@@ -1,31 +1,69 @@
 package com.zebannikolay.capstone;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.zebannikolay.capstone.databinding.ActivityMainBinding;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = item -> {
-                switch (item.getItemId()) {
-                    case R.id.navigation_search:
-                        return true;
-                    case R.id.navigation_recent:
-                        return true;
-                    case R.id.navigation_favorite:
-                        return true;
-                }
-                return false;
-            };
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setTitle(R.string.app_name);
+
+        initBottomNavigation();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    private void initBottomNavigation() {
+        ArrayList<Fragment> fragments = new ArrayList<>(3);
+        binding.viewPager.setAdapter(new ScreenSlidePagerAdapter(getSupportFragmentManager(), fragments));
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_search:
+                binding.viewPager.setCurrentItem(0);
+                return true;
+            case R.id.navigation_recent:
+                binding.viewPager.setCurrentItem(1);
+                return true;
+            case R.id.navigation_favorite:
+                binding.viewPager.setCurrentItem(2);
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.viewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            binding.viewPager.setCurrentItem(binding.viewPager.getCurrentItem() - 1);
+        }
+    }
 }
---
+++ app/src/main/java/com/zebannikolay/capstone/ScreenSlidePagerAdapter.java
@@ -0,0 +1,28 @@
+package com.zebannikolay.capstone;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public ScreenSlidePagerAdapter(@NonNull final FragmentManager fm, @NonNull final List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
