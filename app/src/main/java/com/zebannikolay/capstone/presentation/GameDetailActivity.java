c899968f074c107a5192d564425bf4c7a242dc79
ZebanNikolay <zebannikolay@gmail.com>
2018-07-10 22:59:29 +0700

    Add GameDetailActivity

---
+++ app/src/main/java/com/zebannikolay/capstone/presentation/GameDetailActivity.java
@@ -0,0 +1,43 @@
+package com.zebannikolay.capstone.presentation;
+
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.zebannikolay.capstone.R;
+import com.zebannikolay.capstone.databinding.ActivityGameDetailBinding;
+
+public class GameDetailActivity extends AppCompatActivity {
+
+    private static final String EXTRA_GAME_ID = GameDetailActivity.class.getSimpleName() + "extra_game_id";
+    private ActivityGameDetailBinding binding;
+
+    public static void start(@NonNull final Context context, @NonNull final String gameId) {
+        final Intent intent = new Intent(context, GameDetailActivity.class);
+        intent.putExtra(EXTRA_GAME_ID, gameId);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_game_detail);
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getExtra();
+    }
+
+    private void getExtra() {
+        Intent intent = getIntent();
+        final String gameId = intent.getStringExtra(EXTRA_GAME_ID);
+    }
+}
